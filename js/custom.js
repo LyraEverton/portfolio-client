14e802257a667689a0308e6aec1d5ff9f7b7c4d7
Everton Lyra <e.rodrigo.silva.lyra@accenture.com>
2022-06-19 14:09:24 -0300

    Changed navbar,fixed links and restructured js files

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,4 @@
-
 var totalItems = $('.item').length;
-console.log(totalItems)
 var currentIndex = $('div.active').index() + 1;
 
 $('.num').html('' + currentIndex + '/' + totalItems + '');
@@ -9,3 +7,17 @@ $('#mainSlider').on('slid.bs.carousel', function () {
     currentIndex = $('div.active').index() + 1;
     $('.num').html('' + currentIndex + '/' + totalItems + '');
 });
+
+function scrolldiv(divId) {
+    window.scrollTo(0,
+        findPosition(document.getElementById(divId)));
+}
+function findPosition(obj) {
+    var currenttop = 0;
+    if (obj.offsetParent) {
+        do {
+            currenttop += obj.offsetTop;
+        } while ((obj = obj.offsetParent));
+        return [currenttop];
+    }
+}
